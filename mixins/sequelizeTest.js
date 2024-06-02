f60ecf5e220a048acd2494ec20f64e1e7d1a6336
cho_git <whqjarn111@gmail.com>
2024-06-03 02:55:37 +0900

    주석 추가 및 미사용 코드 주석처리

--- mixins/sequelizeTest.js
+++ mixins/sequelizeTest.js
@@ -1,35 +1,33 @@
 "use strict";
 
-const { ServiceBroker } = require("moleculer");
 const dbService = require("moleculer-db");
 const sqlAdapter = require("moleculer-db-adapter-sequelize");
 const sequelize = require("sequelize");
-const { actions, settings } = require("../services/greeter.service");
 
-const broker = new ServiceBroker();
-
-const seq = new sequelize("testdb", "postgres", "Sikbbang2@", {
-	host: "localhost",
-	port: 5432,
-	dialect: "postgres"
+const seq = new sequelize("postgres", "postgres", "hoomanhoo2@", {
+						//접속할 데이터베이스, DB 계정명, 비밀번호, 
+	host: "localhost",  //DB Server의 Host 정보
+	port: 5432,			// DB Server의 Port 정보
+	dialect: "postgres" // 사용하는 DB의 방언 (dialect) 종류
 });
 
 
 const model = {
-	name: "citiesModel",
-	mixins:[dbService],
-	adapter: new sqlAdapter(seq),
-	model: {
-		name: "cities",
-		define: {
-			name: {
-				type: sequelize.STRING,
+	name: "citiesModel",	//Table의 프로젝트 내에서 사용할 이름을 지정한다. (Table명과는 다름)
+	mixins:[dbService],		// 해당 서비스의 mixin을 지정한다. mixins는 상속과 비슷한 역할을 한다.
+							// mixins에 대한 자세한 내용은 https://razvanpredescu.medium.com/moleculer-mixins-a-short-introduction-a69f053148f5 룰 참고한다.
+	adapter: new sqlAdapter(seq),	//Adapter를 설정한다
+	model: {	//프로젝트에서 사용할 객체 모델을 정의한다.
+		name: "cities",	//Table의 이름을 지정한다
+		define: {		// Table의 네부 속성을 정의한다.
+			name: {		// 컬럼명을 정의한다.
+				type: sequelize.STRING,	// 컬럼의 자료형을 지정한다.
 				primaryKey: true    //명시적으로 PK지정 안하면 "id" 라는 이름의 필드를 찾게 됨 (존재하지 않더라도)
 			},
             location: sequelize.STRING
 			// location: sequelize.GEOMETRY("POINT")  //POSTGRES의 POINT 타입은 GEOMETRY의 하위 분류로 존재
 		},
-		options: {
+		options: {	//기타 설정을 정의한다.
 			sequelize,
 			modelName: "citiesModel",
             timestamps: false   //이 옵션이 없을 경우 Model에서 createAt updateAt 필드를 찾게 됨(존재하지 않더라도)
@@ -37,44 +35,11 @@ const model = {
         
 	},
 	settings: {
-		// idField: "name", //Sequelize Adapter하고는 사용할 수 없음
-		fields: ["name", "location"]
 	},
 	actions: {
-		find: {
-			populate: ["name", "location"],
-			fields: ["name", "location"],
-            searchFields: ["name", "location"]
-		},
-		selectAllCities(ctx){
-			return this.adapter.findAll();
-		}
 	}
 
 };
 module.exports = model;
-// broker.start();
 
-
-
-
-
-// broker.createService({
-// 	name: "cities",
-// 	mixins:[dbService],
-// 	adapter: new sqlAdapter(seq),
-// 	model: {
-// 		name: "cities",
-// 		define: {
-// 			name: sequelize.STRING,
-// 			location: sequelize.GEOMETRY("POINT")  //POSTGRES의 POINT 타입은 GEOMETRY의 하위 분류로 존재
-// 		},
-//         options: {
-//             sequelize,
-//             modelName: "cities"
-//         }
-// 	}
-// });
-
-// broker.start();
 
--- services/newApi.service.js
+++ services/newApi.service.js
@@ -4,23 +4,27 @@ const sequelizeTest = require("../mixins/sequelizeTest");
 const {ServiceBroker} = require("moleculer");
 
 const broker = new ServiceBroker();
-broker.createService(sequelizeTest);
+broker.createService(sequelizeTest);	
+//Service Broker를 생성한 뒤 sequelizeTest를 Service Broker를 통해 newApi.service.js에서 사용할 Service로 등록한다.
 
 module.exports = {
-	name: "new-api",
+	name: "new-api",	//module의 이름을 지정한다.
 	settings: {
-		testsetting: "this is test setting"
+		testsetting: "this is test setting"	//settings에서는 전역적으로 사용할 변수등을 선언할 수 있다.
 	},
-	actions: {
-
+	actions: {	//이 서비스에서 실행할 동작들을 정의한다. 함수형과 key:value 형식으로 정의할 수 있다.
+		
+		//함수형 정의
 		async newAction(ctx){
 			return "this is new action" + this.newPrivateMethod();
 		},
-		async findAllCities(ctx){            
+		async findAllCities(ctx){            	
 			const cities = broker.start().then(() => broker.call("citiesModel.find"));
-
+			// Service Broker로 등록한 sequelizeTest 를 호출하여 find action을 실행한다. find action은 mixins에 설정한 moleculer-db에 정의되어있다.
 			return cities;
 		},
+
+		// key:value 형식 정의
 		insertCityInfo: {
 			params:{
 				name: "string",
@@ -64,7 +68,6 @@ module.exports = {
 			async handler(ctx){
 				const parameters = ctx.params;
 				const cityName = parameters.name;
-				// const cityLocation = parameters.location;
 
 				return broker.start()
 					.then(() => broker.call("citiesModel.remove", {id: cityName}))
@@ -75,33 +78,33 @@ module.exports = {
 
 	},
 	methods: {
-		newPrivateMethod(ctx){
+		newPrivateMethod(ctx){	//methods에서는 이 서비스 내에서만 사용할 수 있는 private Method를 정의할 수 있다.
 			return " and this is new private method";
 		}
-	},
-	events: {
-		"hello.world"(ctx){
-			console.log("Welcome to the world.");
-		}
-	},
-	created(){
-		console.log("The service, ", this.name, "has been created");
-	}, 
-	async started(){
-		// runs when service starts
-		console.log("new-api service is started");
-	},
-	async stopped(){
-		// runs when service stops
-		console.log("new-api service is stopped");
-	},
-	// list of other services this service uses
-	dependencies: [],
-	metadata: {
-		scalable: true,
-		priority: 5
 	}
+	// events: {
+	// 	"hello.world"(ctx){
+	// 		console.log("Welcome to the world.");
+	// 	}
+	// },
+	// created(){
+	// 	console.log("The service, ", this.name, "has been created");
+	// }, 
+	// async started(){
+	// 	// runs when service starts
+	// 	console.log("new-api service is started");
+	// },
+	// async stopped(){
+	// 	// runs when service stops
+	// 	console.log("new-api service is stopped");
+	// },
+	// // list of other services this service uses
+	// dependencies: [],
+	// metadata: {
+	// 	scalable: true,
+	// 	priority: 5
+	// }
 	// Properties available to service instances
 	// https://moleculer.services/docs/0.14/services.html#Properties-of-Service-Instances
 
-}
+};
